f7463b6c873da482db7c48201e9ac3d7b7e2e500
= <=>
2015-09-21 10:45:17 -0400

    Added pies to the mix

---
+++ Pies.java
@@ -0,0 +1,32 @@
+public class Pies {
+	public static void main(String[] args) {
+		//Isaac B Goss 
+		//CS 0401 MW 1:00
+		//So this thing...
+		Scanner sc = new Scanner(System.in);\
+		float[] prices = {10.,12.,2.,50.};
+		//plain pizza, peperoni pizza, cherry pie slice, charm
+		while true {
+			System.out.println("Is there another customer? 1=Yes/0=No");
+			notherCustomer = sc.nextInt();
+			if (notherCustomer==0) {
+				System.exit(0);
+			}
+			do {
+				//Do you have a Lucky Pie Card?
+				//If so, affect prices
+				System.out.println("What are we getting today?");
+				//Print options
+				//get the chosen option, check for invalid numbers
+				//set up if block to pick what cashier chose
+				//if (pizza) { choose number of slices }
+				//if (pie) { if (slices==6) { it's a pie; return 10. } else { return slices*2; } }
+				//add that much to price
+				
+				
+				} while (!moreItems)
+			//math
+			//print out 
+		}
+	}
+}
